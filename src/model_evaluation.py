be122cd13a2d85489ed12de97879f0388d14826e
Swapnasubham Das <swapnasubham1993@gmail.com>
2025-11-09 21:50:20 +0530

    make changes in model_evalution dvlive code

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -123,9 +123,12 @@ def main():
 
         # Experiment tracking using dvclive
         with Live(save_dvc_exp=True) as live:
-            live.log_metric('accuracy', accuracy_score(y_test, y_test))
-            live.log_metric('precision', precision_score(y_test, y_test))
-            live.log_metric('recall', recall_score(y_test, y_test))
+            live.log_metric('accuracy', metrics['accuracy'])
+            live.log_metric('precision', metrics['precision'])
+            live.log_metric('recall', metrics['recall'])
+            live.log_metric('auc', metrics['auc'])  # Optional but useful
+
+            live.log_params(params)
 
             live.log_params(params)
         
